66b4034f06d0fde01ad36c673cee0a5293e95dc2
VSudarshana <viswasudarshana@gmail.com>
2024-05-08 15:26:54 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import ProductDetailsPage  from "./components/ProductDetailsPage ";
 
-const App = () => {
+
+function App() {
   return (
-    <div>App</div>
-  )
+    <Router>
+      <div>
+        <nav className="flex gap-5 justify-end p-6 items-center bg-slate-800 text-white">
+          <Link to="/">Home</Link>
+          <Link to="/product/:productId">Products Details</Link>
+
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/product/:productId" element={<ProductDetailsPage />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+function Home() {
+  return (
+    <>
+     <h3 className="text-center font-bold text-3xl mt-16 text-red-400">WELCOME TO OUR E-COMMERCE PAGE</h3>
+    </>
+  );
+}
+
+export default App;
